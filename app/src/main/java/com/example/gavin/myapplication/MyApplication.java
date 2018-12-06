a54bcd0e360c38f644383d81d064f04bb0acfdce
Gavin <gavin.liu@unisco.com>
2018-12-06 15:08:23 +0800

    second commit

---
+++ app/src/main/java/com/example/gavin/myapplication/MyApplication.java
@@ -0,0 +1,15 @@
+package com.example.gavin.myapplication;
+
+import com.mob.MobApplication;
+
+/**
+ * Created by Gavin
+ */
+
+public class MyApplication extends MobApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
